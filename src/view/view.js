6fe5b6ac260cce001fabd7ec32edd5a9edb44b42
elina <papadelina145@gmail.com>
2023-08-03 11:52:11 +0300

    Hide enemy ships on board

--- src/view/view.js
+++ src/view/view.js
@@ -2,25 +2,30 @@ import Gameboard from '../model/gameboard';
 import Ship from '../model/ship';
 
 export default class View {
-    static generateBoard(id) {
+    static generateBoard(id, board) {
         document.querySelector(id).innerHTML = '';
+        const arrBoard = board.getBoard();
         for (let row = 0; row < 10; row++) {
             for (let col = 0; col < 10; col++) {
                 const square = document.createElement('div');
                 square.classList.add('square');
                 square.dataset.row = row;
                 square.dataset.col = col;
-                square.classList.add('empty');
+                if (id === '#player-board' && arrBoard[row][col] !== '') {
+                    square.classList.add('ship');
+                } else {
+                    square.classList.add('empty');
+                }
                 document.querySelector(id).appendChild(square);
             }
         }
     }
 
     static generatePlayerBoard(board) {
-        View.generateBoard('#player-board');
+        View.generateBoard('#player-board', board);
     }
 
     static generateEnemyBoard(board) {
-        View.generateBoard('#enemy-board');
+        View.generateBoard('#enemy-board', board);
     }
 }
